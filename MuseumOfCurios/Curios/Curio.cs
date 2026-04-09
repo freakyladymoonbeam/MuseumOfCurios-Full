05b33e7099166f02644a948dae2e6679acd140db
jwarnersmith <jwarner1@mycambrian.ca>
2026-04-09 19:48:47 -0400

    Added Origin parameter to curio constructor

--- MuseumOfCurios/Curios/Curio.cs
+++ MuseumOfCurios/Curios/Curio.cs
@@ -9,26 +9,35 @@
         Mythical
     }
 
+    public enum Origin
+    {
+        RollingGrasslands,
+        SunscorchedPlateau,
+        TallPalmTropics,
+        FrostpeakHighlands,
+        SeasAndSands,
+        JadePeaks
+    }
+
     public abstract class Curio
     {
-        public string Name { get; }
-        public string Description { get; }
-        public RarityLevel Rarity { get; }
+        public string Name { get; } // The name of the curio
+        public string Description { get; }  // A brief description of the curio
+        public RarityLevel Rarity { get; } // The rarity level of the curio
+        public Origin Origin { get; } // The region where the curio was found
         public virtual bool IsCustom => false; // Indicates whether this curio is a custom creation, and automatically returns false for all curios that are not explicitly marked as custom
 
-        protected Curio(
-            string name,
-            string description,
-            RarityLevel rarity = RarityLevel.Common)
+        protected Curio(string name, string description, RarityLevel rarity, Origin origin)
         {
             Name = name;
             Description = description;
             Rarity = rarity;
+            Origin = origin;
         }
 
         public virtual string Examine()
         {
-            return $"[{Rarity}] {Name}: {Description}";
+            return $"[{Rarity}] {Name} ({Origin}): {Description}";
         }
     }
 }
